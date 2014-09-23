b67e4b6bfb21f24031cfdd277ae5e799b7843c66
jirachot <jirachot23@gmail.com>
2014-09-23 15:05:59 +0700

    ShowSQL

---
+++ src/app/jirachot/testsqlite/AddActivity.java
@@ -0,0 +1,51 @@
+package app.jirachot.testsqlite;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class AddActivity extends Activity {
+
+	private Button btn_add;
+	private EditText edt_name, edt_tel;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.add);
+
+		btn_add = (Button) findViewById(R.id.btn_add);
+		btn_add.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+
+				SaveData();
+
+			}
+
+		});
+	}
+
+	private void SaveData() {
+
+		edt_name = (EditText) findViewById(R.id.edtname);
+		edt_tel = (EditText) findViewById(R.id.edttel);
+		MydbClass mydbClass = new MydbClass(this);
+
+		long saveStatus = mydbClass.InsertData(edt_name.getText().toString(),
+				edt_tel.getText().toString());
+
+		if (saveStatus <= 0) {
+			Log.d("Error Save", "Error!");
+		}
+
+		Toast.makeText(getApplicationContext(), "Add Data Successfully",
+				Toast.LENGTH_SHORT).show();
+	}
+}
